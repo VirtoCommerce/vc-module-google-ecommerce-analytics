8bcd5b2f4eceacbd8ec0df183b738417649fcb08
alex765022 <alex765022@gmail.com>
2016-07-22 20:09:55 +0200

    manifest settings

--- VirtoCommerce.GoogleEcommerceAnalyticsModule.Web/Module.cs
+++ VirtoCommerce.GoogleEcommerceAnalyticsModule.Web/Module.cs
@@ -1,8 +1,26 @@
-﻿using VirtoCommerce.Platform.Core.Modularity;
+﻿using System.Linq;
+using VirtoCommerce.Platform.Core.Modularity;
+using Microsoft.Practices.Unity;
+using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.GoogleEcommerceAnalyticsModule.Web
 {
     public class Module : ModuleBase
     {
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public override void PostInitialize()
+        {
+            base.PostInitialize();
+
+            var settingManager = _container.Resolve<ISettingsManager>();
+            var googleEcommerceAnalyticsSettings = settingManager.GetModuleSettings("VirtoCommerce.GoogleEcommerceAnalytics").ToArray();
+            settingManager.RegisterModuleSettings("VirtoCommerce.Store", googleEcommerceAnalyticsSettings);
+        }
     }
 }
