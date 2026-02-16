51a427157e642fb45849881d8a270c87a4e66409
Oleg Zhuk <zhukoo@gmail.com>
2026-02-16 11:23:47 +0200

    VCST-4328: Update to .NET10

---
+++ tests/VirtoCommerce.GoogleEcommerceAnalyticsModule.Tests/HelloWorldTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace VirtoCommerce.GoogleEcommerceAnalyticsModule.Tests
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void HelloWorld()
+        {
+        }
+    }
+}
